c154a3b7031b4d88acfb8afb86d40a77c2c0c9cb
pranalighate <46988296+pranalighate@users.noreply.github.com>
2019-04-05 11:22:24 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -11,4 +11,3 @@ do
     echo "www.gamutkart$i container has been created!"
 	echo "=============================="
 done
-docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -a -q` > IPs.txt
