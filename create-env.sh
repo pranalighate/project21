312382849920b26696af12f4dafe6cac1076190b
pranalighate <46988296+pranalighate@users.noreply.github.com>
2019-04-05 11:57:53 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -7,7 +7,7 @@ do
 	echo "=============================="
     echo "Creating www.gamutkart$i container.."
     sleep 1
-sudo docker run --name www.gamutkart$i -d -it --rm pranalighate/png /bin/bash
+sudo docker run -it --name www.gamutkart$i -d --rm pranalighate/png /bin/bash
 echo "www.gamutkart$i container has been created!"
 	echo "=============================="
 done
